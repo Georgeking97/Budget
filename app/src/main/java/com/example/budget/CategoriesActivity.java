664e4f20414af988da7a3526be9524c7e891e1c9
Georgeking97 <44296390+Georgeking97@users.noreply.github.com>
2021-01-03 21:18:43 +0000

    Adding in the ability to play a youtube video

--- app/src/main/java/com/example/budget/CategoriesActivity.java
+++ app/src/main/java/com/example/budget/CategoriesActivity.java
@@ -3,12 +3,51 @@ package com.example.budget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class CategoriesActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+
+public class CategoriesActivity extends YouTubeBaseActivity {
+    private static final String TAG = "CategoriesActivity";
+
+    YouTubePlayerView mYouTubePlayerView;
+    Button btnPlay;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        Log.d(TAG, "onCreate: Starting");
+        btnPlay = findViewById(R.id.btnPlay);
+        mYouTubePlayerView = findViewById(R.id.youtubePlay);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d(TAG, "onClick: Done initializing");
+                youTubePlayer.loadVideo("r9iC-N0_4lY");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d(TAG, "onClick: failed to initializing");
+            }
+        };
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Initializing youtube player");
+                mYouTubePlayerView.initialize(YouTubeConfig.getAPI_KEY(), mOnInitializedListener);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/budget/YouTubeConfig.java
@@ -0,0 +1,13 @@
+package com.example.budget;
+
+public class YouTubeConfig {
+
+    public YouTubeConfig () {
+    }
+
+    private static final String API_KEY = "AIzaSyDvkt2GI7OhcbAPZyjoqmynqQrp9NVk1To";
+
+    public static String getAPI_KEY(){
+        return API_KEY;
+    }
+}
