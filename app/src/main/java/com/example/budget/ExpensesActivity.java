a0de13bb0d71c20c048dd6e5f9ce39c4a13956c4
Georgeking97 <44296390+Georgeking97@users.noreply.github.com>
2021-01-04 17:06:11 +0000

    Final commit

--- app/src/main/java/com/example/budget/ExpensesActivity.java
+++ app/src/main/java/com/example/budget/ExpensesActivity.java
@@ -1,15 +1,54 @@
 package com.example.budget;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ExpensesActivity extends AppCompatActivity {
+    ImageView mImageView;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
 
+        mImageView = findViewById(R.id.image_view);
+        mButton = findViewById(R.id.btn_open);
+        
+        if (ContextCompat.checkSelfPermission(ExpensesActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ExpensesActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            },
+                    100);
+        }
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            mImageView.setImageBitmap(captureImage);
+        }
     }
 }
--- app/src/main/java/com/example/budget/GoalsActivity.java
+++ app/src/main/java/com/example/budget/GoalsActivity.java
@@ -1,16 +1,83 @@
 package com.example.budget;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class GoalsActivity extends AppCompatActivity {
+    private ArrayList<ExampleItem> mExampleList;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button buttonInsert;
+    private Button buttonRemove;
+    private EditText editTextInsert;
+    private EditText editTextRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
 
+        createExampleList();
+        buildRecyclerView();
+
+        buttonInsert = findViewById(R.id.btnInsert);
+        buttonRemove = findViewById(R.id.btnRemove);
+        editTextInsert = findViewById(R.id.edittext_insert);
+        editTextRemove = findViewById(R.id.edittext_remove);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
+
+    }
+
+    public void insertItem (int position) {
+        mExampleList.add(position, new ExampleItem(R.drawable.goals_icon, "Pay off my debt", "Target goal: 5,000"));
+        mAdapter.notifyItemInserted(position);
+    }
+
+    public void removeItem(int position){
+        mExampleList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+    }
+
+    public void createExampleList(){
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.goals_icon, "Buy a house", "Target goal: 50,000"));
+        mExampleList.add(new ExampleItem(R.drawable.goals_icon, "Buy a car", "Target goal: 30,000"));
+        mExampleList.add(new ExampleItem(R.drawable.goals_icon, "Save an emergency fund", "Target goal: 10,000"));
+    }
+
+    public void buildRecyclerView(){
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
